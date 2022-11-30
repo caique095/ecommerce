7a2706bcaa0d9423186ee1da532d38d2fc448ad3
Caique Americo <beatrizbnc@MacBook-Air-de-Beatriz.local>
2022-11-30 14:13:15 -0300

    Finalizando pagina Quem somos

--- src/pages/aboutUs/AboutUs.tsx
+++ src/pages/aboutUs/AboutUs.tsx
@@ -13,30 +13,39 @@ function AboutUs() {
             <div className="">
                 <Container>
                     <section className="d-flex justify-content-center align-center my-4">
-                    <h1 className="fw-semibold">Nossa missão:</h1>  
-                    </section>
-                    
-                    <section className="about-us d-flex justify-content-center align-center text-center">
-                        <article>
-                            <h1 className="fs-4">Lorem Ipsum.</h1>
-                            <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
-                        </article>
 
-                        <article>
-                            <h1 className="fs-4">Lorem Ipsum.</h1>
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
-                        </article>
+                        <h1 className="text-center mt-4" id="nossa_missao"> Nossa missão é levar até sua casa tecnologia, estilo e <span id="nossa_missao-span" className="">muita diversão!</span></h1>
+                    </section>
+                
+
+                    <section id="time">
+                        <section id="time" className="d-flex justify-content-center align-center my-4">
+                            <h1 className="fw-bold my-4" id="time-titulo">Nosso time:</h1>  
+                        </section>
+                        <section className="">
+                            <Team />
+                        </section>
                     </section>
                 </Container>
-
-                <section id="time">
-                    <section id="time" className="d-flex justify-content-center align-center my-4">
-                        <h1 className="fw-semibold">Nosso time:</h1>  
+                
+                <section id="contato" className="">
+                    <section className="d-flex justify-content-center align-center my-4">
+                        <h1 className="fw-bold">Contato:</h1> 
                     </section>
-                    <section className="">
-                        <Team />
+                        
+                    <section className="d-md-flex justify-content-around align-items-center">
+                        <img className="mb-4 d-none d-md-block" src="IMG/contato-img.png" id="contato-img" alt="Foto de contato" />
+                            <div className="d-flex flex-column justify-content-center align-items-center" id="contato-textos">
+                                <Container>
+                                    <h1 className="fs-3 fw-bold my-3">É sempre bom falar com você!</h1>
+                                    <p><span className="fw-semibold">Telefone: </span><a href="tel:+5521912345678" id="contato-msg">(21) 91234-5678</a></p>
+                                    <p><span className="fw-semibold">Email: </span><a href="mailto:contato@lojabezt.com" id="contato-msg">contato@lojabezt.com </a></p>
+                                    <p><span className="fw-semibold">Endereço: </span> Rua Três, Rio de janeiro - RJ</p>
+                                </Container>
+                            </div>
                     </section>
                 </section>
+    
             </div>
 
 </>
